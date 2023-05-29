d1e91548e2ef6558dda8954fcf7f1ed6c33d3703
Hemant Chaudhari <89215477+Hemvc786@users.noreply.github.com>
2023-05-29 12:06:38 +0530

    commiting the project.
    HTML , CSS and Javascript project on Modal window

---
+++ script.js
@@ -0,0 +1,32 @@
+"use script";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+console.log(btnsOpenModal);
+const openModal = function () {
+  console.log("Button Clicked!!!");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  console.log("A key was pressed!!!");
+  console.log(event.key);
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
